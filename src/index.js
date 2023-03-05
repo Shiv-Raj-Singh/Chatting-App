148863cd7f877b70c3ac25f2a16c8edc971d3494
Shiv-Raj-Singh <mt932747@gmail.com>
2023-03-06 01:33:56 +0530

    done

--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ var server = http.createServer(app);
 
 import { Server } from 'socket.io';
 // const io = new Server(server);
-const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  });
+// const io = new Server(server, {
+//     cors: {
+//       // origin: "http://localhost:3000",
+//       methods: ["GET", "POST"]
+//     }
+//   });
 
 app.use(express.json())
 app.use(cors())
