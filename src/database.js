5d8b089fc4cded7085cde4c8304a2976924e7ae4
Shiv-Raj-Singh <mt932747@gmail.com>
2023-03-06 13:58:48 +0530

    cors policy

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 import mongoose from 'mongoose';
 
-
+console.log(process.env.MONGO_DB);
 async function db(){
     mongoose.set('strictQuery', false);
     await mongoose.connect(`${process.env.MONGO_DB}`)
--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import { Server } from 'socket.io';
 const io = new Server(server, {
     cors: {
       // origin: "http://localhost:3000",
-      origin: "https://mangal-chat-app.netlify.app",
+      origin: "*",
       methods: ["GET", "POST"]
     }
   });
